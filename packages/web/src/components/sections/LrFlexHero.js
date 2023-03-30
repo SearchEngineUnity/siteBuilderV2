f4210a9830625ed69a4b43514f3d40b2d46180e9
sc-searchengineunity <sharon@searchengineunity.com>
2023-03-30 15:26:01 -0400

    first attempt of building block selector and uncopling header / footer

--- packages/web/src/components/sections/LrFlexHero.js
+++ packages/web/src/components/sections/LrFlexHero.js
@@ -2,32 +2,75 @@ import React from 'react';
 import Grid from '@mui/material/Grid';
 import ImgBlock from '../blocks/FluidImgBlock';
 import VideoBlock from '../blocks/VideoBlock';
-import SectionBlock from '../blocks/HeroSectionBlock';
-import ButtonAffiliate from '../buttons/ButtonAffiliate';
-import ButtonExternal from '../buttons/ButtonExternal';
-import ButtonInternalGlobal from '../buttons/ButtonInternalGlobal';
-import ButtonInternalLocal from '../buttons/ButtonInternalLocal';
-import ButtonJumpLink from '../buttons/ButtonJumpLink';
+import HeroSectionBlock from '../blocks/HeroSectionBlock';
+import SectionBlock from '../blocks/SectionBlock';
 import GridFlex from '../blocks/BlockGridFlex';
 import BlockFormNetlify from '../blocks/BlockFormNetlify';
 import TestimonialGrid from '../blocks/TestimonialGrid';
 import ClickableImage from '../portableText/insertable/ClickableImage';
 import SmartGridBlock from '../blocks/SmartGridBlock';
+import HeroSectionHeader from './HeroSectionHeader';
+import HeroSectionFooter from './HeroSectionFooter';
 import {
   mapFluidImgBlockToProps,
   mapSectionBlockToProps,
-  mapMuiBtnToProps,
   mapGridFlexToProps,
   mapBlockFormNetlifyToProps,
   mapTestimonialGridToProps,
   mapClickableImageToProps,
   mapSmartGridBlockToProps,
 } from '../../lib/mapToProps';
-import HeroSectionFooter from './HeroSectionFooter';
-import HeroSectionHeader from './HeroSectionHeader';
 import SectionOuterWrapper from './SectionOuterWrapper';
 import SectionInnerWrapper from './SectionInnerWrapper';
+import ConditionalButton from '../buttons/ConditionalButton';
 import { determineColor, lrColCalculator } from '../../lib/helperFunctions';
+
+const blockMapping = {
+  smartGridBlock: SmartGridBlock,
+  videoBlock: VideoBlock,
+  imageBlock: ImgBlock,
+  heroBlock: HeroSectionBlock,
+  sectionBlock: SectionBlock,
+  gridFlex: GridFlex,
+  blockFormNetlify: BlockFormNetlify,
+  clickableImage: ClickableImage,
+  btnBlockMui: ConditionalButton,
+  testimonialGrid: TestimonialGrid,
+};
+
+const propsMapping = (type, data) => {
+  console.log(type);
+  switch (type) {
+    case 'smartGridBlock':
+      return { ...mapSmartGridBlockToProps(data) };
+    case 'videoBlock':
+      return { url: data.url, ratio: data.ratio };
+    case 'imageBlock':
+      return { loading: 'eager', ...mapFluidImgBlockToProps(data) };
+    case 'heroBlock':
+      return { ...mapSectionBlockToProps(data) };
+    case 'sectionBlock':
+      return { ...mapSectionBlockToProps(data) };
+    case 'gridFlex':
+      return { ...mapGridFlexToProps(data) };
+    case 'blockFormNetlify':
+      return { ...mapBlockFormNetlifyToProps(data) };
+    case 'clickableImage':
+      return { ...mapClickableImageToProps(data) };
+    case 'btnBlockMui':
+      return { condition: data.link[0]._type, values: data };
+    case 'testimonialGrid':
+      return { ...mapTestimonialGridToProps(data) };
+    default:
+      return data;
+  }
+};
+
+// issue with blockformnetlify style bgfieldColor
+// issue with clickable image
+// issue with image block
+// issue with smart grid
+// issue with testimonial
 
 function LrFlexHero({
   idTag,
@@ -49,6 +92,18 @@ function LrFlexHero({
   const subtitleColor = determineColor(designSettings?.subtitle?.color) || 'inherit';
   const footerColor = determineColor(designSettings?.footer?.color) || 'inherit';
 
+  const headerFooterProps = {
+    hasSectionHeading: !!heading,
+    hasSectionSubheading: !!subheading,
+    hasSectionFooter: !!footer,
+    hasSectionSubtitle: !!subtitle,
+    headingColor,
+    subheadingColor,
+    subtitleColor,
+    footerColor,
+  };
+
+  console.log(headerFooterProps);
   return (
     <SectionOuterWrapper idTag={idTag} designSettings={designSettings}>
       <SectionInnerWrapper designSettings={designSettings}>
@@ -65,105 +120,41 @@ function LrFlexHero({
           {blocks.map((block, index) => {
             const col = lrColCalculator(colArr[index]);
             const { _type, _key } = block;
-            const blockSelector = (key) => {
-              switch (true) {
-                case key === 'smartGridBlock':
-                  return (
-                    <SmartGridBlock
-                      key={block._key}
-                      hasSectionHeading={!!heading}
-                      hasSectionSubheading={!!subheading}
-                      hasSectionFooter={!!footer}
-                      hasSectionSubtitle={!!subtitle}
-                      headingColor={headingColor}
-                      subheadingColor={subheadingColor}
-                      subtitleColor={subtitleColor}
-                      footerColor={footerColor}
-                      {...mapSmartGridBlockToProps(block)}
-                    />
-                  );
-                case key === 'videoBlock':
-                  return <VideoBlock key={_key} url={block.url} ratio={block.ratio} />;
-                case key === 'imageBlock':
-                  return (
-                    <ImgBlock {...mapFluidImgBlockToProps(block)} key={_key} loading="eager" />
-                  );
-                case key === 'heroBlock':
-                  return (
-                    <SectionBlock
-                      hasSectionHeading={!!heading}
-                      hasSectionSubheading={!!subheading}
-                      hasSectionFooter={!!footer}
-                      hasSectionSubtitle={!!subtitle}
-                      headingColor={headingColor}
-                      subheadingColor={subheadingColor}
-                      subtitleColor={subtitleColor}
-                      footerColor={footerColor}
-                      {...mapSectionBlockToProps(block)}
-                    />
-                  );
-                case key === 'gridFlex':
-                  return (
-                    <GridFlex
-                      key={block._key}
-                      hasSectionHeading={!!heading}
-                      hasSectionSubheading={!!subheading}
-                      hasSectionFooter={!!footer}
-                      hasSectionSubtitle={!!subtitle}
-                      headingColor={headingColor}
-                      subheadingColor={subheadingColor}
-                      subtitleColor={subtitleColor}
-                      footerColor={footerColor}
-                      {...mapGridFlexToProps(block)}
-                    />
-                  );
-                case key === 'blockFormNetlify':
-                  return (
-                    <BlockFormNetlify key={block._key} {...mapBlockFormNetlifyToProps(block)} />
-                  );
-                case key === 'clickableImage':
-                  return <ClickableImage {...mapClickableImageToProps(block)} />;
-                case key === 'btnBlockMui' && block.link[0]._type === 'jumpLink':
-                  return <ButtonJumpLink key={_key} {...mapMuiBtnToProps(block)} />;
-                case key === 'btnBlockMui' && block.link[0]._type === 'affiliateLink':
-                  return <ButtonAffiliate key={_key} {...mapMuiBtnToProps(block)} />;
-                case key === 'btnBlockMui' && block.link[0]._type === 'externalLink':
-                  return <ButtonExternal key={_key} {...mapMuiBtnToProps(block)} />;
-                case key === 'btnBlockMui' && block.link[0]._type === 'internalGlobal':
-                  return <ButtonInternalGlobal key={_key} {...mapMuiBtnToProps(block)} />;
-                case key === 'btnBlockMui' && block.link[0]._type === 'internalLocal':
-                  return <ButtonInternalLocal key={_key} {...mapMuiBtnToProps(block)} />;
-                case key === 'testimonialGrid':
-                  return (
-                    <TestimonialGrid
-                      key={_key}
-                      hasSectionHeading={!!heading}
-                      hasSectionSubheading={!!subheading}
-                      hasSectionFooter={!!footer}
-                      hasSectionSubtitle={!!subtitle}
-                      headingColor={headingColor}
-                      subheadingColor={subheadingColor}
-                      subtitleColor={subtitleColor}
-                      footerColor={footerColor}
-                      {...mapTestimonialGridToProps(block)}
-                    />
-                  );
-                default:
-                  return <div key="default-inner-block"> Block still under development</div>;
-              }
-            };
-            return (
+            const Block = blockMapping[_type];
+            const values = propsMapping(_type, block);
+
+            return Block ? (
               <Grid
                 item
                 {...col}
-                key={block._key}
+                key={_key}
                 sx={[
                   index === 0 && reverseOrder && { order: { xs: 2, sm: 1 } },
                   index === 1 && reverseOrder && { order: { xs: 1, sm: 2 } },
                 ]}
               >
-                {blockSelector(_type)}
+                <HeroSectionHeader
+                  heading={heading}
+                  subheading={subheading}
+                  subtitle={subtitle}
+                  align={headerAlignment}
+                  hasSectionHeading={!!heading}
+                  hasSectionSubheading={!!subheading}
+                  hasSectionSubtitle={!!subtitle}
+                  headingColor={headingColor}
+                  subheadingColor={subheadingColor}
+                  subtitleColor={subtitleColor}
+                />
+                <Block {...values} />
+                <HeroSectionFooter
+                  footer={footer}
+                  footerColor={footerColor}
+                  align={footerAlignment}
+                  hasSectionFooter={!!footer}
+                />
               </Grid>
+            ) : (
+              <div>undefined block</div>
             );
           })}
         </Grid>
