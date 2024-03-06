51a3287626725d6765e14dcc878fb64d4e56adec
sc-searchengineunity <sharon@searchengeineunity.com>
2024-03-06 09:32:40 -0500

    new component for Main Column Guide Hero

---
+++ packages/web/src/components/sections/MainColumnGuideHero.js
@@ -0,0 +1,67 @@
+import React, { forwardRef } from 'react';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Unstable_Grid2/Grid2';
+import Typography from '@mui/material/Typography';
+import Subtitle from '../portableText/serializer/H1SubtitleSerializer';
+import HeroTags from './HeroTags';
+import Breadcrumbs from '../navs/breadcrumbs/SpgHeroBreadcrumbs';
+
+const MainColumnGuideHeroWithRef = forwardRef(function MainColumnGuideHero(
+  { h1, subtitle, date, author, topicTags, primarySubcategory, secondarySubcategory },
+  ref,
+) {
+  const lastUpdatedDate = date ? new Date(date.replace(/-/g, '/')) : null;
+  const options = { year: 'numeric', month: 'long', day: 'numeric' };
+
+  return (
+    <Box
+      ref={ref}
+      sx={{
+        bgcolor: 'transparent',
+        color: 'text.primary',
+        pt: { xs: '16px', md: '40px' },
+        mb: '12px',
+      }}
+      id="hero"
+      component="header"
+    >
+      <Container maxWidth="lg">
+        <Grid container direction="row" spacing={3}>
+          <Grid>
+            {primarySubcategory && <Breadcrumbs primarySubcategory={primarySubcategory} />}
+          </Grid>
+          <Grid>
+            <Typography variant="h1">{h1}</Typography>
+          </Grid>
+          <Grid md={9} xs={12}>
+            {subtitle && (
+              <Box sx={{ mb: '24px' }}>
+                <Subtitle blocks={subtitle} />
+              </Box>
+            )}
+
+            <Box sx={{ mb: '24px' }}>
+              <HeroTags
+                topicTags={topicTags}
+                primarySubcategory={primarySubcategory}
+                secondarySubcategory={secondarySubcategory}
+              />
+            </Box>
+
+            <Typography variant="body1" component="p">
+              {author && <Box component="span">By {author} | </Box>}
+              {lastUpdatedDate && (
+                <Box component="span" color="text.secondary">
+                  Updated on {lastUpdatedDate.toLocaleDateString('en-US', options)}
+                </Box>
+              )}
+            </Typography>
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
+  );
+});
+
+export default MainColumnGuideHeroWithRef;
