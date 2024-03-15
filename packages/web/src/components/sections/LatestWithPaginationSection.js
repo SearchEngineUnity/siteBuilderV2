f1b1d052a1b90202bf26c1919bf8d2f5a2f0d21b
sc-searchengineunity <sharon@searchengeineunity.com>
2024-03-15 16:06:03 -0400

    lay initial ground work for all the data required for section

--- packages/web/src/components/sections/LatestWithPaginationSection.js
+++ packages/web/src/components/sections/LatestWithPaginationSection.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import Grid from '@mui/material/Unstable_Grid2/Grid2';
+import { Link } from 'gatsby';
+import Pagination from '@mui/material/Pagination';
+import PaginationItem from '@mui/material/PaginationItem';
 import StructuredSectionFooter from './StructuredSectionFooter';
 import StructuredSectionHeader from './StructuredSectionHeader';
 import SectionOuterWrapper from './SectionOuterWrapper';
@@ -15,16 +18,22 @@ function LatestWithPaginationSection({
   headerAlignment,
   footerAlignment,
   designSettings,
+  spgTilesContent,
+  numPages,
+  currentpage,
+  slug,
 }) {
   const headingColor = determineColor(designSettings?.heading?.color) || 'inherit';
   const subheadingColor = determineColor(designSettings?.subheading?.color) || 'inherit';
   const subtitleColor = determineColor(designSettings?.subtitle?.color) || 'inherit';
   const footerColor = determineColor(designSettings?.footer?.color) || 'inherit';
 
+  console.log(spgTilesContent);
+
   return (
     <SectionOuterWrapper idTag={idTag} designSettings={designSettings}>
       <SectionInnerWrapper designSettings={designSettings}>
-        <Grid container alignItems="center" spacing={6} direction="column">
+        <Grid container spacing={6} direction="column">
           {(heading || subheading || subtitle) && (
             <Grid xs={12}>
               <StructuredSectionHeader
@@ -38,7 +47,38 @@ function LatestWithPaginationSection({
               />
             </Grid>
           )}
-          <Grid>Latest with Pagination Section</Grid>
+          <Grid container spacing={6}>
+            {spgTilesContent.map((tile) => (
+              <Grid key={tile.node.slug.current}>
+                <div>{tile.node.slug.current}</div>
+                <div>{tile.node.displayDate}</div>
+                <div>
+                  {tile.node.hero.feature === 'video' && tile.node.hero?.video?.url
+                    ? 'spg has video'
+                    : 'no video'}
+                </div>
+              </Grid>
+            ))}
+          </Grid>
+          {numPages > 1 && (
+            <Grid xs={12}>
+              <Pagination
+                variant="outlined"
+                shape="rounded"
+                showFirstButton
+                showLastButton
+                page={currentpage}
+                count={numPages}
+                renderItem={(item) => (
+                  <PaginationItem
+                    component={Link}
+                    to={`/${slug}${item.page === 1 ? '' : `/${item.page}`}`}
+                    {...item}
+                  />
+                )}
+              />
+            </Grid>
+          )}
           {footer && (
             <Grid xs={12}>
               <StructuredSectionFooter
