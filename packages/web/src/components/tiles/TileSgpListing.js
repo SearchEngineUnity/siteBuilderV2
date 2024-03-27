68de47f58e4dd6dcc72b0a7a6008644679746842
sc-searchengineunity <sharon@searchengeineunity.com>
2024-03-27 12:22:25 -0400

    new tile for listing sgps

---
+++ packages/web/src/components/tiles/TileSgpListing.js
@@ -0,0 +1,136 @@
+// Tile #7
+import React from 'react';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import { GatsbyImage } from 'gatsby-plugin-image';
+import { Link } from 'gatsby-theme-material-ui';
+import PlayCircleIcon from '@mui/icons-material/PlayCircle';
+
+function TileSgpListing({ image, tag, title, slug, hasVideo }) {
+  const link = slug === '/' ? '/' : `/${slug}`;
+  return (
+    <Card
+      sx={{
+        borderRadius: '12px',
+        height: '100%',
+        color: (theme) => theme.palette.text.primary,
+        '&:hover': {
+          boxShadow:
+            'rgba(0, 0, 0, 0.2) 0px 2px 1px -1px, rgba(0, 0, 0, 0.25) 4px 6px 5px 0px, rgba(0, 0, 0, 0.12) 0px 1px 3px 0px',
+          color: (theme) => theme.palette.primary.main,
+        },
+        '&:hover img': {
+          transform: 'scale(1.1)',
+          transition: 'transform 0.5s ease',
+        },
+      }}
+      elevation={1}
+    >
+      <Link
+        to={link}
+        underline="none"
+        sx={{
+          display: 'flex',
+          flexDirection: 'row',
+          alignItems: 'flex-start',
+          flexWrap: 'wrap',
+          height: { sm: '100%', xs: '112px' },
+          color: 'inherit',
+          '&:focus': {
+            boxShadow:
+              'rgba(0, 0, 0, 0.2) 0px 2px 4px -1px, rgba(0, 0, 0, 0.14) 0px 4px 5px 0px, rgba(0, 0, 0, 0.12) 0px 1px 10px 0px',
+            color: (theme) => theme.palette.primary.main,
+          },
+          '&:focus img': {
+            transform: 'scale(1.1)',
+          },
+        }}
+      >
+        <Box
+          sx={{
+            position: 'relative',
+            height: {
+              sm: '200px',
+              xs: '112px',
+            },
+            width: {
+              sm: '100%',
+              xs: '33%',
+            },
+          }}
+        >
+          <Box
+            sx={{
+              position: 'absolute',
+              zIndex: 1,
+              color: 'common.white',
+              bgcolor: 'common.white',
+              bottom: '4px',
+              right: '4px',
+              width: '48px',
+              height: '48px',
+              borderRadius: '12px',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+          >
+            {hasVideo && (
+              <PlayCircleIcon
+                sx={{
+                  color: 'common.white',
+                  bgcolor: 'common.black',
+                  borderRadius: '50%',
+                  width: '36px',
+                  height: '36px',
+                }}
+              />
+            )}
+          </Box>
+          <GatsbyImage
+            image={image}
+            alt=""
+            objectFit="cover"
+            style={{ width: '100%', height: '100%' }}
+          />
+        </Box>
+        <CardContent sx={{ width: { sm: '100%', xs: '67%' }, height: '100%' }}>
+          <Typography
+            color="primary"
+            variant="body2"
+            component="div"
+            fontWeight="fontWeightBold"
+            textTransform="uppercase"
+            sx={{
+              overflow: 'hidden',
+              textOverflow: 'ellipsis',
+              display: '-webkit-box',
+              WebkitLineClamp: { xs: 1, sm: 'unset' },
+              WebkitBoxOrient: 'vertical',
+            }}
+          >
+            {tag}
+          </Typography>
+          <Typography
+            variant="h5"
+            component="div"
+            sx={{
+              overflow: 'hidden',
+              textOverflow: 'ellipsis',
+              display: '-webkit-box',
+              WebkitLineClamp: { xs: 3, sm: 'unset' },
+              WebkitBoxOrient: 'vertical',
+              color: 'inherit',
+            }}
+          >
+            {title}
+          </Typography>
+        </CardContent>
+      </Link>
+    </Card>
+  );
+}
+
+export default TileSgpListing;
