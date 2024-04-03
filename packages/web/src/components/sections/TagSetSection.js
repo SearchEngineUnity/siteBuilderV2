b40c2b38d3943a11ed4950ab7db1736dd0ec9ce6
sc-searchengineunity <sharon@searchengeineunity.com>
2024-04-03 14:11:09 -0400

    update spacing

--- packages/web/src/components/sections/TagSetSection.js
+++ packages/web/src/components/sections/TagSetSection.js
@@ -27,7 +27,7 @@ function TagSetSection({
   return (
     <SectionOuterWrapper idTag={idTag} designSettings={designSettings}>
       <SectionInnerWrapper designSettings={designSettings}>
-        <Grid container alignItems="center" spacing={6} direction="column">
+        <Grid container alignItems="center" spacing={{ xs: 2, sm: 3 }} direction="column">
           {(heading || subheading || subtitle) && (
             <Grid xs={12}>
               <StructuredSectionHeader
