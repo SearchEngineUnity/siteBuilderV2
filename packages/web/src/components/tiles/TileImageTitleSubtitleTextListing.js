a3628b1404d03e932727d8cb1bc918e173fce755
sc-searchengineunity <sharon@searchengeineunity.com>
2024-04-09 12:04:31 -0400

    remove unused import

--- packages/web/src/components/tiles/TileImageTitleSubtitleTextListing.js
+++ packages/web/src/components/tiles/TileImageTitleSubtitleTextListing.js
@@ -2,7 +2,6 @@
 import React from 'react';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import { getGatsbyImageData } from 'gatsby-source-sanity';
 import { GatsbyImage } from 'gatsby-plugin-image';
