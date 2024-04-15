6391f8bcfc470a53a6137a5798f9210fe4e051e1
sc-searchengineunity <sharon@searchengeineunity.com>
2024-04-15 12:27:10 -0400

    add more use client directive to client components

--- packages/web/src/components/blocks/TestimonialCarousel.jsx
+++ packages/web/src/components/blocks/TestimonialCarousel.jsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React from 'react';
 import Box from '@mui/material/Box';
 import Carousel from 'react-material-ui-carousel';
--- packages/web/src/components/sections/MainColumnGuideHero.jsx
+++ packages/web/src/components/sections/MainColumnGuideHero.jsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React, { forwardRef } from 'react';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
