22383d07ded2e5e12c91589b372e91e16cb21b50
sc-searchengineunity <sharon@searchengeineunity.com>
2024-04-23 14:13:26 -0400

    comment out unused footer code

--- packages/web/src/components/SocialMedia.jsx
+++ packages/web/src/components/SocialMedia.jsx
@@ -1,47 +1,48 @@
-import React from 'react';
-import { IconButton } from 'gatsby-theme-material-ui';
-import Facebook from '@mui/icons-material/Facebook';
-import Instagram from '@mui/icons-material/Instagram';
-import LinkedIn from '@mui/icons-material/LinkedIn';
-import Pinterest from '@mui/icons-material/Pinterest';
-import Twitter from '@mui/icons-material/Twitter';
-import YouTube from '@mui/icons-material/YouTube';
-import HomeIcon from '@mui/icons-material/Home';
-import { useSocialInfo } from '../hooks/useSocialInfo';
+// file not in use
+// import React from 'react';
+// import { IconButton } from 'gatsby-theme-material-ui';
+// import Facebook from '@mui/icons-material/Facebook';
+// import Instagram from '@mui/icons-material/Instagram';
+// import LinkedIn from '@mui/icons-material/LinkedIn';
+// import Pinterest from '@mui/icons-material/Pinterest';
+// import Twitter from '@mui/icons-material/Twitter';
+// import YouTube from '@mui/icons-material/YouTube';
+// import HomeIcon from '@mui/icons-material/Home';
+// import { useSocialInfo } from '../hooks/useSocialInfo';
 
-const socialNetworks = {
-  facebook: Facebook,
-  twitter: Twitter,
-  instagram: Instagram,
-  pinterest: Pinterest,
-  linkedin: LinkedIn,
-  youtube: YouTube,
-  homestars: HomeIcon,
-};
+// const socialNetworks = {
+//   facebook: Facebook,
+//   twitter: Twitter,
+//   instagram: Instagram,
+//   pinterest: Pinterest,
+//   linkedin: LinkedIn,
+//   youtube: YouTube,
+//   homestars: HomeIcon,
+// };
 
-export default function SocialMedia() {
-  const socials = useSocialInfo();
+// export default function SocialMedia() {
+//   const socials = useSocialInfo();
 
-  return (
-    <>
-      {socials.map((social) => {
-        const SocialSelected = socialNetworks[social.node.social];
-        if (SocialSelected) {
-          return (
-            <IconButton
-              color="inherit"
-              to={social.node.link}
-              target="_blank"
-              key={social.node.social}
-              aria-label={`${social.node.social}`}
-              size="large"
-            >
-              <SocialSelected />
-            </IconButton>
-          );
-        }
-        return <div key={social.node.social}>Social Media Error</div>;
-      })}
-    </>
-  );
-}
+//   return (
+//     <>
+//       {socials.map((social) => {
+//         const SocialSelected = socialNetworks[social.node.social];
+//         if (SocialSelected) {
+//           return (
+//             <IconButton
+//               color="inherit"
+//               to={social.node.link}
+//               target="_blank"
+//               key={social.node.social}
+//               aria-label={`${social.node.social}`}
+//               size="large"
+//             >
+//               <SocialSelected />
+//             </IconButton>
+//           );
+//         }
+//         return <div key={social.node.social}>Social Media Error</div>;
+//       })}
+//     </>
+//   );
+// }
--- packages/web/src/hooks/useContactInfo.jsx
+++ packages/web/src/hooks/useContactInfo.jsx
@@ -1,23 +1,24 @@
-/* eslint-disable import/prefer-default-export */
-import { useStaticQuery, graphql } from 'gatsby';
+// file not in use
+// /* eslint-disable import/prefer-default-export */
+// import { useStaticQuery, graphql } from 'gatsby';
 
-export const useContactInfo = () => {
-  const { info } = useStaticQuery(
-    graphql`
-      query contactInfo {
-        info: sanityContactInfo {
-          address1
-          address2
-          city
-          country
-          email
-          phone
-          mailCode
-          provinceState
-        }
-      }
-    `,
-  );
+// export const useContactInfo = () => {
+//   const { info } = useStaticQuery(
+//     graphql`
+//       query contactInfo {
+//         info: sanityContactInfo {
+//           address1
+//           address2
+//           city
+//           country
+//           email
+//           phone
+//           mailCode
+//           provinceState
+//         }
+//       }
+//     `,
+//   );
 
-  return info;
-};
+//   return info;
+// };
