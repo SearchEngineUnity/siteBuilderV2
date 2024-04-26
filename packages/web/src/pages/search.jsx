3dcb4e30e65a8c6c51f40bc85e1dabb197a99f83
sc-searchengineunity <sharon@searchengeineunity.com>
2024-04-26 14:54:44 -0400

    update page content width + add layout component

--- packages/web/src/pages/search.jsx
+++ packages/web/src/pages/search.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import algoliasearch from 'algoliasearch/lite';
 import { InstantSearch, Index } from 'react-instantsearch';
-import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import CustomSearchBox from '../components/search/customSearchBox';
 import HitsSwitcher from '../components/search/hitsSwitcher';
 import QueryDisplay from '../components/search/queryDisplay';
+import MyLayout from '../containers/layout';
 
 const searchClient = algoliasearch(
   process.env.GATSBY_ALGOLIA_APP_ID,
@@ -14,21 +15,37 @@ const searchClient = algoliasearch(
 
 function Search() {
   return (
-    <Container maxWidth="lg">
-      <Typography variant="h1" textAlign="center" sx={{ padding: '1em 0' }}>
-        Search
-      </Typography>
-      <InstantSearch searchClient={searchClient} indexName="pro_SGP">
-        <CustomSearchBox />
-        <QueryDisplay />
-        <Index indexName="pro_Listing">
-          <HitsSwitcher />
-        </Index>
-        <Index indexName="pro_SGP">
-          <HitsSwitcher />
-        </Index>
-      </InstantSearch>
-    </Container>
+    <MyLayout>
+      <Box
+        component="main"
+        sx={{
+          padding: '0 16px',
+          width: '100%',
+          margin: '0 auto',
+          '@media (min-width: 600px)': {
+            padding: '0 24px',
+          },
+          '@media (min-width: 782px)': {
+            maxWidth: '782px',
+          },
+          minHeight: 'calc(100vh - 316px)',
+        }}
+      >
+        <Typography variant="h1" textAlign="center" sx={{ padding: '1em 0' }}>
+          Search
+        </Typography>
+        <InstantSearch searchClient={searchClient} indexName="pro_SGP">
+          <CustomSearchBox />
+          <QueryDisplay />
+          <Index indexName="pro_Listing">
+            <HitsSwitcher />
+          </Index>
+          <Index indexName="pro_SGP">
+            <HitsSwitcher />
+          </Index>
+        </InstantSearch>
+      </Box>
+    </MyLayout>
   );
 }
 
