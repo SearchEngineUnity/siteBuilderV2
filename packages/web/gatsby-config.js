980f49cbe6f0c018c2cab737880d1b46881e27f1
sc-searchengineunity <sharon@searchengeineunity.com>
2024-05-22 15:56:41 -0400

    remove gatsby favicon

--- packages/web/gatsby-config.js
+++ packages/web/gatsby-config.js
@@ -102,9 +102,9 @@ module.exports = {
     'gatsby-plugin-sitemap',
     {
       resolve: 'gatsby-plugin-manifest',
-      options: {
-        icon: 'src/images/icon.png',
-      },
+      // options: {
+      //   icon: 'src/images/icon.png',
+      // },
     },
     `gatsby-plugin-netlify`,
     {
