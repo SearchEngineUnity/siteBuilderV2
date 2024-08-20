67670c2e6c615cbc7eccf98cddcd0f9f50ebacbc
sc-searchengineunity <sharon@searchengeineunity.com>
2024-08-20 15:12:24 -0400

    relable relatedITems field in relatedCotnentSection

--- packages/studio/schemas/sections/relatedContentSection.js
+++ packages/studio/schemas/sections/relatedContentSection.js
@@ -10,7 +10,7 @@ export default {
     },
     {
       name: 'relatedItems',
-      title: 'Best Related Guides',
+      title: 'Guides',
       type: 'array',
       of: [{ type: 'reference', to: [{ type: 'soloGuidePage' }] }],
       validation: (Rule) => Rule.max(4).error('Maximum 4 items'),
